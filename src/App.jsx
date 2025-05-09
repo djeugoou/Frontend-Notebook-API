ed2614029267ded797d65f48b3e5390a03ba5737
djeugoou <djeugoustella@gmail.com>
2025-05-09 22:30:50 +0200

    delete, put request

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import axios from 'axios';
+
 import {
   Button,
   DatePicker,
@@ -8,7 +9,8 @@ import {
   Form,
   Input,
   Space,
-   Table, 
+  Table,
+  Modal, 
   Tag,
 } from 'antd';
 const { RangePicker } = DatePicker;
@@ -27,41 +29,133 @@ const formItemLayout = {
 const { Column, ColumnGroup } = Table;
 
 const App = () => {
-
-  
-  const [data, setData] = useState(null)
-  const [form] = Form.useForm();
-  const variant = Form.useWatch('variant', form);
-
-  //POST
-  const handleSubmit=(values)=>{
-  axios.post('http://localhost:8000/notes', values)
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.log(error);
+  function SendGetRequest(url){
+    axios.get(url)
+    .then(function (response) {
+      setData(response.data)     
   })
   }
+  const [id,setId] =useState(0)
+  const [isModalOpen, setIsModalopen] = useState(false)
+  const [formData, setFormData] = useState({"title":"1", "content":"","category":"","icon":"","duedate":""})
+  const [data, setData] = useState(null)
+  const [form] = Form.useForm();
+  const [formEdit]= Form.useForm();
+  const [deleteRefreshTrigger,setDeleteRefreshTrigger]=useState(false)
+  const [postRefreshTrigger,setPostRefreshTrigger]=useState(false)
+  const [updateRefreshTrigger,setupdateRefreshTrigger]=useState(false)
 
-  //Get request
-
-   axios.get('http://localhost:8000/notes')
-  .then(function (response) {
-    console.log(response.data);
-    setData(response.data) //this is what displays the user input on frontend
+  console.log 
+  //POST Request
+ const handleSubmit=(values)=>{
+    axios.post('http://localhost:8000/notes', values)
+    .then((response) => {
+      response.json()
+    })
+    .catch((error) => {
+      console.log(error);
   })
+    setPostRefreshTrigger(!postRefreshTrigger)
+    form.resetFields()//reset Form fields
+  }
 
-  //Delete Request
-  axios.delete('http://localhost:8000/notes{id}',)
+  useEffect(()=>{
+    SendGetRequest('http://localhost:8000/notes')
+    },[])
 
+  useEffect(()=>{
+    SendGetRequest('http://localhost:8000/notes')
+  },[postRefreshTrigger])
 
+   useEffect(()=>{
+    SendGetRequest('http://localhost:8000/notes')
+  },[updateRefreshTrigger])
+
+   useEffect(()=>{
+    SendGetRequest('http://localhost:8000/notes')
+    },[deleteRefreshTrigger])
+
+  const openModal=(id,record)=>{
+    setFormData(record)
+    setIsModalopen(true)
+    setId(id)
+  }
+  function sendUpdate(id, values) {
+    axios.put('http://localhost:8000/notes/'+id, values)
+    .then(res => {
+      message.success('Note updated');
+      // e.g. reload notes or update local state
+    })
+    .catch(err => {
+      message.error(`Update failed (${err.response?.status})`);
+    });
+}
+
+  const handleDelete=(id)=>{
+     axios.delete('http://localhost:8000/notes/'+id)
+      .then(res => {
+
+    if (res.status === 204) {
+      // update table state…
+      setDeleteRefreshTrigger(!deleteRefreshTrigger)
+    }
+  })
+  .catch(err => {
+    console.error('error status:', err.response?.status);
+  });
+}
  
+ const handleCancel=()=>{
+  setIsModalopen(false)
+ }
+ const handleOk=()=>{
+  sendUpdate(id,formData)
+  setIsModalopen(false)
+  setupdateRefreshTrigger(!updateRefreshTrigger)
+ }
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setFormData(formdata => ({ ...formdata, [name]: value }));
+  };
+
+
   return (
     <>
+    <Modal
+        title="Edit"
+        open={isModalOpen}
+        okText='Save'
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Form
+            labelCol={{span:5}}
+            form={formEdit}
+        >
+          <Form.Item label="Title" rules={[{ required: true, message: 'Please input!' }]}>
+            <Input value={formData.title}  name='title' onChange={handleInputChange}/>
+           </Form.Item>
+           <Form.Item label="Content"  rules={[{ required: true, message: 'Please input!' }]}>
+              <Input  value={formData.content} name='content'onChange={handleInputChange}/>
+            </Form.Item>
+           <Form.Item label="Category" rules={[{ required: true, message: 'Please input!' }]}>
+              <Input   value={formData.category} name='category'onChange={handleInputChange}/>
+           </Form.Item>
+            <Form.Item label="Icon" rules={[{ required: true, message: 'Please input!' }]}>
+              <Input   value={formData.icon} name='icon'onChange={handleInputChange}/>
+            </Form.Item>
+            <Form.Item label="Due Date" rules={[{ required: true, message: 'Please input!' }]}>
+              <Input   value={formData.duedate} name='duedate'onChange={handleInputChange}/>
+            </Form.Item>
+        </Form>
+        
+    </Modal>
     <Form 
+      title='Create New Node'
       style={{ maxWidth: 600 }}
       onFinish={handleSubmit}
+      form={form}
+      labelCol={{span:5}}
     >
       <Form.Item label="Title" name="title" rules={[{ required: true, message: 'Please input!' }]}>
         <Input />
@@ -110,8 +204,6 @@ const App = () => {
       </Form.Item>
     </Form>
 
-    //***************************Table+++++++ */
-
      <Table dataSource={data}>
     
     <Column title="Title" dataIndex="title" key="title" />
@@ -123,17 +215,21 @@ const App = () => {
     <Column
       title="Action"
       key="action"
-      render={(_, record) => (
+      render={(_, record) =>(
         <Space size="middle">
-          <a>Edit{record.lastName}</a>
-           <a>Delete</a>
+           <Button type="primary" htmlType="submit" onClick={()=>openModal(record.id,record)}>
+          Edit
+        </Button>
+            <Button type="primary" htmlType="submit" onClick={()=>handleDelete(record.id)} >
+          Delete
+        </Button>
         </Space>
       )}
     />
   </Table>
   </>
   
-    
-  )}
+  )
+}
 
-export default App;
+export default App
